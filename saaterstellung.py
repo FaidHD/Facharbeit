df78b2de4162572bdf938516d7b4f9c44d45a769
KeksBombe <43473527+KeksBombe@users.noreply.github.com>
2021-11-26 12:42:12 +0100

    kjbgfiu

--- saaterstellung.py
+++ saaterstellung.py
@@ -1,6 +1,13 @@
 import sqlite3 as sql
 
+# Verbindung zur Datenbank erzeugen
+connection = sqlite3.connect("Saatgut.db")
+
+# Datensatz-Cursor erzeugen
+cursor = connection.cursor()
+
 def create(name, wachzeit, kornabstand, reihenabstand):
     sql = "INSERT INTO saat VALUES(name, wachszeit, kornabstand, reihenabstand)"
     cursor.execute(sql)
     connection.commit()
+    connection.close()
