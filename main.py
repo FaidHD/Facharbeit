9080289e8f20c7524a7e70e3e1a72ae5ef6fe8ae
KeksBombe <43473527+KeksBombe@users.noreply.github.com>
2021-11-26 20:48:27 +0100

    removed print statment(again)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def SeedInput():
     wachszeit = int(input("Wachszeit: "))
     kornabstand = int(input("Kornabstand: "))
     reihenabstand = int(input("Reihenabstand: "))
-    return (saE.create(name, wachszeit, kornabstand, reihenabstand))
+    saE.create(name, wachszeit, kornabstand, reihenabstand)
 
 
 def sven():
@@ -48,7 +48,7 @@ def start():
             os.remove("Saatgut.db")
         except:
             print("No old Database found, creating new one")
-        print(cDB.Database())
+        cDB.Database()
         start()
     else:
         print("please Enter a Valid Number. Press Enter to try again.")
@@ -60,7 +60,7 @@ if os.path.exists("Saatgut.db"):
     print("Database already created!")
     start()
 else:
-    print(cDB.Database())
+    cDB.Database()
     start()
 
 
